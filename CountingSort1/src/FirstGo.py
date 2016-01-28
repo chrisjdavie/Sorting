b099d07a599d5bff102bc304b78066b522f7a107
chrisjdavie <chris.d@theasi.co>
2016-01-28 11:42:06 +0000

    starting the hackerrank counting sort puzzle

---
+++ CountingSort1/src/FirstGo.py
@@ -0,0 +1,14 @@
+'''
+Created on 28 Jan 2016
+
+@author: chris
+'''
+
+N = input()
+ar = map(int,raw_input().strip().split())
+
+counts = [0]*N
+print len(counts)
+print counts
+counts[0] = 1
+print counts
