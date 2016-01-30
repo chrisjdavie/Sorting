3ebc86462c7bfb431559ad78a89afd6e7bcfdd52
chrisjdavie <chris.d@theasi.co>
2016-01-30 17:21:52 +0000

    Started the counting sort 2 challenge

---
+++ CountingSort2/src/FirstGo.py
@@ -0,0 +1,30 @@
+'''
+Solving the hackerrank Counting Sort 2 challenge
+
+https://www.hackerrank.com/challenges/countingsort2
+
+----------------------
+
+Often, when a list is sorted, the elements being sorted are just keys to other values. For example, if you are sorting files by their size, the sizes need to stay connected to their respective files. You cannot just take the size numbers and output them in order, you need to output all the required file information.
+
+However, if you are not concerned about any other information, then you can simply sort those numbers alone. This makes counting sort very simple. If you already counted the values in the list, you don't need to access the original list again. This challenge involves a simple counting sort where the elements being sorted are all that matter.
+
+Challenge 
+Given an unsorted list of integers, output the integers in order.
+
+Hint: You can use your previous code that counted the items to print out the actual values in order.
+
+Input Format 
+There will be two lines of input:
+
+n - the size of the list
+ar - n space separated numbers that belong to the list
+Output Format 
+Output all the numbers of the list in order.
+
+----------------------
+
+Created on 30 Jan 2016
+
+@author: chris
+'''
