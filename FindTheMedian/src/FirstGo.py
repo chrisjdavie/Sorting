b868e18a49e65e896514f079addcdf7183286b0b
chrisjdavie <chris.d@theasi.co>
2016-01-31 18:15:22 +0000

    Solving the hackerrank find the median puzzle

---
+++ FindTheMedian/src/FirstGo.py
@@ -0,0 +1,28 @@
+'''
+Solving the hackerrank Find The Median puzzle.
+
+https://www.hackerrank.com/challenges/find-median
+
+It will take longer to build this package than solve.
+
+----------------------
+
+n the Quicksort challenges, you sorted an entire array. Sometimes, you just need specific information about a list of numbers, and doing a full sort would be unnecessary. Can you figure out a way to use your partition code to find the median in an array?
+
+Challenge 
+Given a list of numbers, can you find the median?
+
+Input Format 
+There will be two lines of input:
+
+n - the size of the array
+ar - n numbers that makes up the array
+Output Format 
+Output one integer, the median.
+
+----------------------
+
+Created on 31 Jan 2016
+
+@author: chris
+'''
